85e6fc06bf7cebcea2aba696bed42391b251d6ea
Dario Lombardo <dario.lombardo@telecomitalia.it>
2018-01-19 16:25:22 +0100

    spdx: rework the script and add more licenses.

--- spdx/spdx.rb
+++ spdx/spdx.rb
@@ -5,20 +5,52 @@ if ARGV.size == 0
 	exit 1
 end
 
-licenses = {}
+licenses = []
 
 Dir.glob("#{File.dirname($0)}/licenses/*").each do |file|
-	licenses[File.basename(file)] = File.read(file)
+	tokens = File.read(file).split("---")
+	text = tokens.first
+	spdx = tokens.last.strip
+	if !spdx
+		puts "Licence file #{file} is invalid, skipping."
+		next
+	end
+	
+	texts = licenses.map{|lic| lic[:text] }
+	if texts.include?(text)
+		puts "Licence from file #{file} is duplicated"
+		next
+	end
+
+	puts "Loading license #{spdx} from #{file}"
+	licenses << { spdx: spdx, text: text }
 end
 
 Dir.glob("#{ARGV.first}/*").each do |filename|
 	next if File.directory?(filename)
-	licenses.each_pair do |spdx, license|
-		content = File.read(filename)
-		if content[0..1000].include?(license)
-			puts "Fixing file #{File.basename(filename)} with spdx #{spdx}"
-			placeholder = " * SPDX-License-Identifier: #{spdx}"
-			File.open(filename, 'w').write(content.gsub(license, placeholder))
+	
+	content = File.read(filename)
+
+	needs = true
+
+	if content[0..1000].include?('SPDX-License-Identifier')
+		needs = false
+	end
+
+	licenses.each do |license|
+		# puts "AAAAAAAAAAAAAAAAAAAA"
+		# puts content[0..1000]
+		# puts "BBBBBBBBBBBBBBBBBBBBBB"
+		# puts license[:text]
+		# puts "CCCCCCCCCCCCCCCCCCCCC"
+		if content[0..3000].include?(license[:text])
+			puts "\tPatching #{filename} with spdx #{license[:spdx]}"
+			File.open(filename, 'w').write(content.gsub(license[:text], license[:spdx]))
+			needs = false
 		end
+	end
+
+	if needs
+		puts "\t############## #{filename} needs SPDX"
 	end
 end

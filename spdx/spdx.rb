2945116c8b0304ee49e8fb5bfa048fcc9ea32b34
Dario Lombardo <dario.lombardo@telecomitalia.it>
2018-01-19 12:31:53 +0100

    spdx: rework the changer.

--- spdx/spdx.rb
+++ spdx/spdx.rb
@@ -1,14 +1,24 @@
 #!/usr/bin/env ruby
 
 if ARGV.size == 0
-	puts "Usage: #{$0} <source file> <licence file> <spdx file>"
+	puts "Usage: #{$0} <root dir>"
 	exit 1
 end
 
-source = File.read(ARGV[0])
-licence = File.read(ARGV[1])
-spdx = File.read(ARGV[2])
+licenses = {}
 
-source.gsub!(licence, spdx)
+Dir.glob("#{File.dirname($0)}/licenses/*").each do |file|
+	licenses[File.basename(file)] = File.read(file)
+end
 
-File.open(ARGV[0], 'w').write(source)
+Dir.glob("#{ARGV.first}/*").each do |filename|
+	next if File.directory?(filename)
+	licenses.each_pair do |spdx, license|
+		content = File.read(filename)
+		if content[0..1000].include?(license)
+			puts "Fixing file #{File.basename(filename)} with spdx #{spdx}"
+			placeholder = " * SPDX-License-Identifier: #{spdx}"
+			File.open(filename, 'w').write(content.gsub(license, placeholder))
+		end
+	end
+end

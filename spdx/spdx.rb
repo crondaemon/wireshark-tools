a79204dd91d462f4b65802b2bf52627bb8d358d5
Dario Lombardo <dario.lombardo@telecomitalia.it>
2018-02-02 12:09:23 +0100

    spdx: more licenses added.

--- spdx/spdx.rb
+++ spdx/spdx.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'string-similarity'
+
 if ARGV.size == 0
 	puts "Usage: #{$0} <root dir>"
 	exit 1
@@ -9,11 +11,11 @@ licenses = []
 
 Dir.glob("#{File.dirname($0)}/licenses/*").each do |file|
 	tokens = File.read(file).split("---")
-	text = tokens.first
-	spdx = tokens.last.strip
-	if !spdx
+	text = tokens.first#.gsub(/\n$/,'')
+	spdx = tokens.last.gsub("\n",'')
+	if !spdx || !spdx.include?('SPDX-License-Identifier')
 		puts "Licence file #{file} is invalid, skipping."
-		next
+		exit 1
 	end
 	
 	texts = licenses.map{|lic| lic[:text] }
@@ -23,31 +25,39 @@ Dir.glob("#{File.dirname($0)}/licenses/*").each do |file|
 	end
 
 	puts "Loading license #{spdx} from #{file}"
-	licenses << { spdx: spdx, text: text }
+	licenses << { spdx: spdx, text: text, filename: file }
 end
 
-Dir.glob("#{ARGV.first}/*").each do |filename|
+Dir.glob("#{ARGV.first}/**/*").each do |filename|
 	next if File.directory?(filename)
+	next if filename.include?('build-')
+	next if filename.include?('Make')
+	next if filename.match(/.l$/)
+	next if File.basename(filename).match(/^README/)
 	
 	content = File.read(filename)
 
 	needs = true
 
-	if content[0..1000].include?('SPDX-License-Identifier')
-		needs = false
-	end
-
-	licenses.each do |license|
-		# puts "AAAAAAAAAAAAAAAAAAAA"
-		# puts content[0..1000]
-		# puts "BBBBBBBBBBBBBBBBBBBBBB"
-		# puts license[:text]
-		# puts "CCCCCCCCCCCCCCCCCCCCC"
-		if content[0..3000].include?(license[:text])
-			puts "\tPatching #{filename} with spdx #{license[:spdx]}"
-			File.open(filename, 'w').write(content.gsub(license[:text], license[:spdx]))
-			needs = false
+	if !content[0..1000].include?('SPDX-License-Identifier')
+		licenses.each do |license|
+			# puts "AAAAAAAAAAAAAAAAAAAA"
+			# puts content[0..1000]
+			# puts "BBBBBBBBBBBBBBBBBBBBBB"
+			# puts license[:text]
+			# puts "CCCCCCCCCCCCCCCCCCCCC"
+			if content[0..3000].include?(license[:text])
+				puts "\tPatching #{filename} with spdx #{license[:spdx]} (#{license[:filename]})"
+				File.open(filename, 'w').write(content.gsub(license[:text], license[:spdx]))
+				needs = false
+			else
+				if String::Similarity.cosine(content[0..3000], license[:text]) > 0.99
+					puts ">>>>>>>>>> Guess: #{license[:spdx]}"
+				end
+			end
 		end
+	else
+		needs = false
 	end
 
 	if needs
